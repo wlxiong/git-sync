0e3d462c760fd2182cafc38a7e05c6210bcaabd4
Xiong Yiliang <xiongyiliang@xunlei.com>
2015-04-23 17:37:54 +0800

    randomize test cases

--- test/run-test.sh
+++ test/run-test.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/sh -ex
 
 DIR="$(dirname $0)"
 cd "$DIR"
@@ -13,7 +13,6 @@ update_repos() {
     append-file.sh
     git-commit.sh
     popd
-    sleep 1
 }
 
 merge_file() {
@@ -24,37 +23,54 @@ merge_file() {
     popd
 }
 
+do_sync() {
+    local HOSTS="$1"
+    local ALL=$2
+    for host0 in $HOSTS; do
+        for host1 in $HOSTS; do
+            if [ "$host0" = "$host1" ]; then
+                continue
+            fi
+            if [ $ALL -eq 1 -o $(($RANDOM % 2)) -eq 1 ]; then
+                sync-repos.sh $host0 $host1
+            fi
+            if [ $ALL -eq 1 -o $(($RANDOM % 2)) -eq 1 ]; then
+                sync-repos.sh $host1 $host0
+            fi
+        done
+    done
+}
+
+check_sync() {
+    local HOSTS="$1"
+    for host0 in $HOSTS; do
+        for host1 in $HOSTS; do
+            if [ "$host0" = "$host1" ]; then
+                continue
+            fi
+            diff -r --exclude=".git" "$host0.git" "$host1.git"
+        done
+    done
+}
+
 rm -rf HostA.git HostB.git HostC.git
 init-repos.sh HostA.git
 init-repos.sh HostB.git
 init-repos.sh HostC.git
 
-for i in {1..2}; do
+for i in {1..5}; do
     update_repos HostA
     update_repos HostB
     update_repos HostC
 
-    # sync: HostA <-> HostB
-    sync-repos.sh HostA HostB
-    sync-repos.sh HostB HostA
-    # sync: HostA <-> HostC
-    sync-repos.sh HostA HostC
-    sync-repos.sh HostC HostA
-    # sync: HostB <-> HostC
-    sync-repos.sh HostB HostC
-    sync-repos.sh HostC HostB
+    do_sync "HostA HostB HostC" 0
 
     merge_file HostA
     merge_file HostB
     merge_file HostC
 
-    # sync: HostA <-> HostB
-    sync-repos.sh HostA HostB
-    sync-repos.sh HostB HostA
-    # sync: HostA <-> HostC
-    sync-repos.sh HostA HostC
-    sync-repos.sh HostC HostA
-    # sync: HostB <-> HostC
-    sync-repos.sh HostB HostC
-    sync-repos.sh HostC HostB
+    do_sync "HostC HostA HostC" 0
 done
+
+do_sync "HostC HostB HostA" 1
+check_sync "HostC HostB HostA"
